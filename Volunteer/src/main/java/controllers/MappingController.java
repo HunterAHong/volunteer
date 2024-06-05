7116e9d48da8279e1b6a17115a3c46b55ced427b
Hunter Hong <hhong@bowdoin.edu>
2024-06-05 12:44:17 +0530

    [Implementation] Stubs for APIController, MappingController, and index.html

---
+++ Volunteer/src/main/java/controllers/MappingController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for url mappings. Returns the appropriate HTML
+ * page in the /src/main/resources/templates folder.
+ */
+@Controller
+public class MappingController {
+    /**
+     * On a GET request to /index, the IndexController will return
+     * /src/main/resources/templates/index.html.
+     *
+     * @param model underlying UI model
+     * @return contents of the page
+     */
+    @GetMapping({ "/index", "/index.html", "/" })
+    public String index(final Model model) {
+        return "index";
+    }
+}
