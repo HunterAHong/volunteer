1fe807f6f870977549ac978404f995787a8c87a3
Hunter Hong <hhong@bowdoin.edu>
2024-06-05 16:37:23 +0530

    [Controller] base package scan and @RestController tweaks - working index blank html

--- Volunteer/src/main/java/com/hhong/volunteer/VolunteerApplication.java
+++ Volunteer/src/main/java/com/hhong/volunteer/VolunteerApplication.java
@@ -2,20 +2,13 @@ package com.hhong.Volunteer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@RestController
+@ComponentScan(basePackages = "controllers")
 public class VolunteerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VolunteerApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 }
--- Volunteer/src/main/java/controllers/MappingController.java
+++ Volunteer/src/main/java/controllers/MappingController.java
@@ -3,12 +3,13 @@ package controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for url mappings. Returns the appropriate HTML
  * page in the /src/main/resources/templates folder.
  */
-@Controller
+@RestController
 public class MappingController {
     /**
      * On a GET request to /index, the IndexController will return
