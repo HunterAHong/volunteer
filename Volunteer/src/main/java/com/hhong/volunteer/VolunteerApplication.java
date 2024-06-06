6fd9eaf514fad4be2a4b046d8f61d2554d4865bd
Hunter Hong <hhong@bowdoin.edu>
2024-06-06 11:18:38 +0530

    [Config] switched to application.yml, successful boot and connection to db

--- Volunteer/src/main/java/com/hhong/volunteer/VolunteerApplication.java
+++ Volunteer/src/main/java/com/hhong/volunteer/VolunteerApplication.java
@@ -3,11 +3,12 @@ package com.hhong.Volunteer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "controllers")
+@PropertySource("classpath:application.yml")
 public class VolunteerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VolunteerApplication.class, args);
 	}
