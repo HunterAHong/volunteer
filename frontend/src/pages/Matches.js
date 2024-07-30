0f1b495ad18a6c16cc26adcbecb99e8b75b5facb
Hunter Hong <hhong@bowdoin.edu>
2024-07-30 11:49:36 -0700

    [Frontend] matches are added to logged in user on button click, and displayed

--- frontend/src/pages/Matches.js
+++ frontend/src/pages/Matches.js
@@ -2,22 +2,29 @@ import React, { useEffect } from "react"
 import { useState } from "react"
 import { FaPlus } from 'react-icons/fa'
 import { useAuth } from '../contexts/AuthContext'
+import { Card } from "react-bootstrap"
 
 export default function Matches({ API_URL }) {
   const [users, setUsers] = useState([])
   const { currentUser } = useAuth()
+  const [matches, setMatches] = useState()
 
 
   useEffect(() => {
     let ignore = false
-    console.log(API_URL)
     const fetchUsers = async () => {
       try {
         const response = await fetch(API_URL)
         const userList = await response.json()
-        console.log(userList)
         console.log(currentUser.email)
         setUsers(userList)
+
+        const userResponse = await fetch(API_URL + "/palid2@outlook.com")
+        const user = await userResponse.json()
+        console.log(user)
+
+        fetchMatches()
+
       } catch (err) {
         console.log(err.stack)
       }
@@ -27,8 +34,34 @@ export default function Matches({ API_URL }) {
     return () => { ignore = true }
   }, [API_URL])
 
-  const addMatch = (phone) => {
-    console.log(phone)
+  const fetchMatches = async () => {
+    try {
+      console.log(currentUser.email)
+      const response = await fetch("http://localhost:8080/api/v1/matches/" + currentUser.email)
+      console.log(response)
+      console.log(response.headers)
+      setMatches(await response.json())
+      console.log("This is matches" + matches)
+    } catch (err) {
+      console.log(err.stack)
+    }
+  }
+
+  const addMatch = async (email) => {
+    console.log(email)
+    // put match
+    const response = fetch("http://localhost:8080/api/v1/matches/" + currentUser.email, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(email)
+    })
+
+    console.log(response)
+    console.log((await response).type)
+
+    fetchMatches()
   }
 
   return (
@@ -37,16 +70,29 @@ export default function Matches({ API_URL }) {
       <div>
         {users.map(user => (
           <div key={user.id}>{user.first} {user.last}
-            <button onClick={() => addMatch(user.phoneNumber)}
+            <button onClick={() => addMatch(user.email)}
               title='Match'
               type='submit'
               aria-label='Enter Name'
             ><FaPlus /></button>
           </div>
-
         ))}
-      </div>
 
+        <Card className="d-flex align-items-center justify-content-center">
+          <Card.Body>
+            <h5 className="card-title">
+              Your Matched
+            </h5>
+
+            <p>
+              {matches}
+            </p>
+            {/* {matches.map(matches => (
+              <div key={matches.id}> {matches.email}</div>
+            ))} */}
+          </Card.Body>
+        </Card>
+      </div>
 
     </main>)
 
